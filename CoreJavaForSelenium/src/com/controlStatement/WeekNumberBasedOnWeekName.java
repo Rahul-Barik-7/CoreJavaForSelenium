9578b6274eb7e75801501f1170ab194e42dbd7ea
91707 <rahulbarik481@gmail.com>
2024-09-19 01:30:38 +0530

    Just Checking the Change history

--- CoreJavaForSelenium/src/com/controlStatement/WeekNumberBasedOnWeekName.java
+++ CoreJavaForSelenium/src/com/controlStatement/WeekNumberBasedOnWeekName.java
@@ -7,14 +7,14 @@ public class WeekNumberBasedOnWeekName {
 		
 		switch(week)
 		{
-		case "sunday":System.out.println("1");break;
-		case "monday":System.out.println("2");break;
-		case "tuesday":System.out.println("3");break;
-		case "wednesday":System.out.println("4");break;
-		case "thursday":System.out.println("5");break;
-		case "friday":System.out.println("6");break;
-		case "saturday":System.out.println("7");break;
-		default:System.out.println(week+"-Please enter a valid week name in lowercase letter");
+		case "Sunday":System.out.println("1");break;
+		case "Monday":System.out.println("2");break;
+		case "Tuesday":System.out.println("3");break;
+		case "Wednesday":System.out.println("4");break;
+		case "Thursday":System.out.println("5");break;
+		case "Friday":System.out.println("6");break;
+		case "Saturday":System.out.println("7");break;
+		default:System.err.println(week+"-Please enter a valid week name (The 1st character should be uppercase)");
 		}
 		
 	}
