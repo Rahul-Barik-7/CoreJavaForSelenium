dfd6e560cd98779fc224209f19c614f673599aa1
91707 <rahulbarik481@gmail.com>
2024-10-22 23:55:17 +0530

    commit

---
+++ CoreJavaForSelenium/src/com/Constructor/RecursiveConstructorCall.java
@@ -0,0 +1,21 @@
+package com.Constructor;
+
+public class RecursiveConstructorCall {
+	
+	
+	//recursive constructor call is not possible
+	
+	RecursiveConstructorCall()
+	{
+		//this(10);
+	}
+	RecursiveConstructorCall(int a)
+	{
+		//this();
+	}
+	
+	public static void main(String[] args) 
+	{	
+		System.out.println("hello");
+	}
+}
---
+++ CoreJavaForSelenium/src/com/String/subString.java
@@ -0,0 +1,12 @@
+package com.String;
+
+public class subString {
+	
+	//it is used to extract substring from main string
+	
+	public static void main(String[] args) {
+		
+		String s=new String();
+		s="";
+	}
+}
