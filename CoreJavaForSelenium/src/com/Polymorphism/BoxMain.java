170142781039097a17fc02ce383151d33eebf9a7
91707 <rahulbarik481@gmail.com>
2025-01-27 21:50:46 +0530

    2nd coommit

---
+++ CoreJavaForSelenium/src/com/Polymorphism/BoxMain.java
@@ -0,0 +1,18 @@
+package com.Polymorphism;
+
+public class BoxMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Box b=new Box();
+//		System.out.println(b.volume());
+		
+//		Box b=new Box(10.5,20.5,30.5);
+//		System.out.println(b.volume());
+		
+		Box b=new Box(10.5);
+		System.out.println(b.volume());
+	}
+
+}
